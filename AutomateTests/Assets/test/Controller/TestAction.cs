3424fdd48a5cb7cb1f31394fee7a5c47f100d154
donman2007 <donman2007@gmail.com>
2017-03-20 01:07:01 +0200

    place tests in correct place

---
+++ AutomateTests/Assets/test/Controller/TestAction.cs
@@ -0,0 +1,18 @@
+﻿using Assets.src.Controller;
+using Assets.src.Controller.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutomateTests.test.Controller
+{
+    [TestClass()]
+    public class TestGameTestAction
+    {
+        [TestMethod]
+        public void TestGetID_ExpectNotNull()
+        {
+            var action = new MasterTestMasterAction(ActionType.AreaSelection);
+            Assert.IsNotNull(action.Id);
+
+        }
+    }
+}
