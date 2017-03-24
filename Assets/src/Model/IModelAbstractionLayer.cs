5da16a6e4aaeae86c88f25ab019e394f6ae21d65
nglevy <nathanglevy@gmail.com>
2017-03-24 17:40:30 +0300

    defined model abstraction layer

--- Assets/src/Model/IModelAbstractionLayer.cs
+++ Assets/src/Model/IModelAbstractionLayer.cs
@@ -5,6 +5,18 @@ namespace Assets.src.Model
 {
     public interface IModelAbstractionLayer
     {
-        MovementPath GetMovementPath();
+        long CreateMovable(long WorldId, Coordinate spawnCoordinate);
+        void IssueMoveCommand(long WorldId, long movableId, Coordinate targetCoordinate);
+        bool IsMovableInMotion(long WorldId, long movableId);
+        MovementPackage GetNextMovement(long WorldId);
+        MovementPackage MoveMovableToNext(long WorldId, long moveableId);
+        double GetMovableSpeed(long WorldId, long moveableId);
+        Coordinate GetMovableCurrentCoordinate(long WorldId, long moveableId);
+    }
+
+    public struct MovementPackage
+    {
+        Coordinate targetCoordinate;
+        Movement movement;
     }
 }
