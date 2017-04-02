8add31e742208f05487cea6e15398d98734c9ab6
nglevy <nathanglevy@gmail.com>
2017-04-02 19:40:23 +0300

    changes to view

--- Assets/src/View/DummyBehaviour.cs
+++ Assets/src/View/DummyBehaviour.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class DummyBehaviour : MonoBehaviour
 {
-    public GameObject prefabGameObject;
+    public GameObject TilePrefabGameObject;
+    public GameObject MovablePrefabGameObject;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,32 +13,50 @@ public class DummyBehaviour : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-	    if (Input.GetKeyDown(KeyCode.Q) == true)
+        Vector2 spriteSize = TilePrefabGameObject.GetComponent<SpriteRenderer>().sprite.rect.size;
+        Vector2 localSpriteSize = spriteSize / TilePrefabGameObject.GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
+        Vector3 worldSize = localSpriteSize;
+        worldSize.x *= transform.lossyScale.x;
+        worldSize.y *= transform.lossyScale.y;
+        if (Input.GetKeyDown(KeyCode.Q) == true)
 	    {
             Debug.Log("Pressed Q - Generating map items");
             GameObject mapParentObject = new GameObject("GameWorldMapContainer");
 
             for (int x = 0; x < 10; x++)
 	        {
-	            for (int y = 0; y < 10; y++)
+	            for (int y = 0; y < 20; y++)
 	            {
-	                Vector2 spriteSize = prefabGameObject.GetComponent<SpriteRenderer>().sprite.rect.size;
-                    Vector2 localSpriteSize = spriteSize / prefabGameObject.GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
-                    Vector3 worldSize = localSpriteSize;
-                    worldSize.x *= transform.lossyScale.x;
-                    worldSize.y *= transform.lossyScale.y;
+
 //	                Vector3 screenSize = 0.5f * worldSize / Camera.main.orthographicSize;
 //	                screenSize.y *= Camera.main.aspect;
 //
 //                    Debug.Log( "localSpriteSize: " + localSpriteSize);
 //                    Debug.Log( "worldSize: " + worldSize);
 //                    Debug.Log( "screenSize: " + screenSize);
-	                GameObject newGameObject = Instantiate(prefabGameObject, new Vector3(x * localSpriteSize.x, y * localSpriteSize.y, 0), Quaternion.identity);
+	                GameObject newGameObject = Instantiate(TilePrefabGameObject, new Vector3(x * localSpriteSize.x, y * localSpriteSize.y, 0), Quaternion.identity);
 	                newGameObject.name = "MapCell_" + x + "_" + y;
                     newGameObject.transform.parent = mapParentObject.transform;
 	            }
 	        }
         }
 
+	    if (Input.GetKeyDown(KeyCode.W) == true)
+	    {
+            Debug.Log("Pressed W - Generating a movable at mouse");
+            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+	        Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject newGameObject = Instantiate(MovablePrefabGameObject, getCellPositionFromVector(currentMousePosition, worldSize), Quaternion.identity);
+        }
+
+    }
+
+    Vector3 getCellPositionFromVector(Vector3 input, Vector3 worldSizeTile)
+    {
+        input += worldSizeTile / 2;
+        input.x = input.x - ((input.x ) % worldSizeTile.x);
+        input.y = input.y - ((input.y ) % worldSizeTile.y);
+        input.z = -2;
+        return input;
     }
 }
